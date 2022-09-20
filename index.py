3d4590234de5c01b9bc5053b5f85a0d168897322
muslim17n <muslim7bek@email.com>
2022-09-20 17:48:40 +0600

    first commit

---
+++ index.py
@@ -0,0 +1,4 @@
+# import main
+# main.add(20,40)
+# main.sub(10,5)
+# print(main.it)
---
+++ main.py
@@ -0,0 +1,8 @@
+# def add(num1,num2):
+#     print(num1+num2)
+# def sub(num1,num2):
+#     print(num1-num2)
+# it="It-Run"
+# def students(*names):
+#     print(names)
+from main import add,sub,it
